079f2990ad36ffeea11db78bd019ec4c7fe3dec3
hfz1337 <gh_zouahi@esi.dz>
2021-05-29 21:54:17 +0100

    style: add missing type hints

--- eruditus/eruditus.py
+++ eruditus/eruditus.py
@@ -90,13 +90,14 @@ async def on_ready() -> None:
 
 
 @bot.event
-async def on_guild_join(guild) -> None:
+async def on_guild_join(guild: Guild) -> None:
+    """Sets up a database for the newly joined guild."""
     await setup_database(guild)
     logger.info(f"{bot.user} joined {guild}!")
 
 
 @bot.event
-async def on_guild_remove(guild) -> None:
+async def on_guild_remove(guild: Guild) -> None:
     """Deletes the database for the guild we just left."""
     mongo.drop_database(f"{DBNAME_PREFIX}-{guild.id}")
     logger.info(f"{bot.user} left {guild}.")
