5ae644cecb70283d0b02039409d762f37136a849
hfz <gh_zouahi@esi.dz>
2022-04-19 01:18:33 -0400

    Add lib/types.py

---
+++ eruditus/lib/types.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class CPUArchitecture(Enum):
+    x86 = 1
+    x64 = 2
+    arm = 3
+    armthumb = 4
+
+
+class EncodingOperationMode(Enum):
+    encode = 1
+    decode = 2
+
+
+class ArchiveMode(Enum):
+    minimal = 1
+    all = 2
+
+
+class CTFStatusMode(Enum):
+    active = 1
+    all = 2
+
+
+class NoteType(Enum):
+    progress = 1
+    note = 2
+
+
+class NoteFormat(Enum):
+    embed = 1
+    raw = 2
